1807259d5a550a072a08488b715983d80572ffd5
DouglasLopesDev <douglas.web.dl@gmail.com>
2023-05-03 20:16:30 -0300

    levando para novo repositório

---
+++ Sender/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Sender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escreva sua mensagem: ");        
+            var message = Console.ReadLine();
+
+            while (message != "exit"){
+                // message = Console.ReadLine();
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+                using (var connection = factory.CreateConnection())
+                {
+                    using (var channel = connection.CreateModel())
+                    {
+                        channel.QueueDeclare("AV AX4B", false, false, false, null);
+
+
+                        var body = Encoding.UTF8.GetBytes(message);
+
+                        channel.BasicPublish("", "AV AX4B", null, body);
+                        Console.WriteLine(" [x] Sent {0}", message);
+                    }
+                }
+                Console.WriteLine(" Digite 'exit' para sair.");
+                message = Console.ReadLine();
+            }
+
+            // var child_ids = 2;
+            // Console.WriteLine("Responsible");
+            // Console.WriteLine(child_ids == 0 ? "RESPONSIBLE VAZIO" : "RESPONSIBLE PREENCHIDO");
+            // Console.WriteLine("check box" );
+            // if(child_ids != 1) {
+            //     Console.WriteLine(child_ids == 0 ? "LICENCE VAZIO" : (child_ids == 2 ? "LICENCE PREENCHIDO" : ""));
+            // }
+        }
+    }
+}
---
+++ Sender_API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Sender_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("get")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost]
+        [Route("message")]
+        public object PostMessage()
+        {
+            var factory = new ConnectionFactory() {
+                HostName = "localhost"
+            };
+       
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare("AV AX4B", false, false, false, null);
+
+                    var rng = new Random();
+
+                    var weatherForecast = new WeatherForecast
+                    {
+                        Date = DateTime.Now,
+                        TemperatureC = rng.Next(-20, 55),
+                        Summary = Summaries[rng.Next(Summaries.Length)]
+                    };
+
+                    var weatherForecastJson = JsonConvert.SerializeObject(weatherForecast);
+
+                    var body = Encoding.UTF8.GetBytes(weatherForecastJson);
+
+                    channel.BasicPublish("", "AV AX4B", null, body);
+                }
+            }
+
+            return new { Message= "Endpoint finalizado"+ AmqpTcpEndpoint.UseDefaultPort };
+        }
+    }
+}
